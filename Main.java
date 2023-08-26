c7d0045010df1c11cb80f771f835ba2d0ca6d7df
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-25 22:19:54 -0700

    main method implemented

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Solution sol = new Solution();
+
+        System.out.println(sol.maxProfit(new int[] { 7, 1, 5, 3, 6, 4 }));
+        System.out.println(sol.maxProfit(new int[] {7, 6, 4, 3, 1}));
     }
 }
