cf568840a00f8b5da23af94f071e76fb4cab036e
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-25 22:31:27 -0700

    solution found

--- Solution.java
+++ Solution.java
@@ -1,5 +1,12 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        return 0;
+        int minimum = Integer.MAX_VALUE;
+        int delta = Integer.MIN_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            minimum = Math.min(minimum, prices[i]);
+            delta = Math.max(delta, prices[i] - minimum);
+        }
+        return delta;
     }
 }
